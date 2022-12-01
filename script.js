00842160a674aa984e0a9dfac48abb72f31f5bed
Sandra Eng <sandra.eng@live.no>
2022-12-01 11:38:28 +0100

    Added comments

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 var xstart = 200;
 var ystart = 200;
-var størrelse = 10;
+var størrelse = 20;
 var xhas = størrelse;
 var yhas = 0;
 var score = 0;
@@ -14,40 +14,48 @@ class Kroppsdel {
     this.farge = farge;
   }
 
+  //Draw bodyparts
   tegnKvadrat() {
     fill("#3B69A9");
     stroke("#192954");
-    strokeWeight(5);
+    strokeWeight(10);
     square(this.xpos, this.ypos, this.stø);
   }
 
+  //Draw food
   tegnKvadratM() {
     fill("#3B69A9");
     stroke("#192954");
-    strokeWeight(5);
+    strokeWeight(10);
     square(this.xpos, this.ypos, this.stø);
   }
 }
 
+//The 3 first body parts
 var slange = [
   new Kroppsdel(xstart, ystart, størrelse, "red"),
   new Kroppsdel(xstart - størrelse, ystart, størrelse, "white"),
   new Kroppsdel(xstart - 2 * størrelse, ystart, størrelse, "blue"),
-];
+]
+//First food location
 var mat = new Kroppsdel(xstart + 100, ystart, størrelse, "red");
 
+//The setup function
 function setup() {
   createCanvas(500, 500);
   frameRate(framerate);
 }
 
+//The draw function
 function draw() {
   background("#3B69A9");
 
+  //Score text
   stroke(25, 41, 84, 60);
   fill(25, 41, 84, 50);
   textSize(200);
 
+  //Score location
   if (score < 10) {
     text(score, 195, 300);
   } else if (score >= 10 && score < 100) {
@@ -56,12 +64,15 @@ function draw() {
     text(score, 75, 300);
   }
 
+  //Draw the snake
   for (let i = 0; i < slange.length; i = i + 1) {
     slange[i].tegnKvadrat();
   }
 
+  //Draw the food
   mat.tegnKvadratM();
 
+  //Death
   for (let i = 1; i < slange.length; i = i + 1) {
     if (slange[0].xpos == slange[i].xpos && slange[0].ypos == slange[i].ypos) {
       frameRate(0);
@@ -70,23 +81,25 @@ function draw() {
     }
   }
 
+  //Get bigger
   if (slange[0].xpos == mat.xpos && slange[0].ypos == mat.ypos) {
     slange.unshift(
       new Kroppsdel(slange[0].xpos + xhas, slange[0].ypos + yhas, størrelse)
     );
     mat.xpos = Math.floor(random(0, 480));
-    mat.xpos = mat.xpos / 10;
+    mat.xpos = mat.xpos / størrelse;
     mat.xpos = Math.floor(mat.xpos);
-    mat.xpos = mat.xpos * 10;
+    mat.xpos = mat.xpos * størrelse;
     mat.ypos = Math.floor(random(0, 480));
-    mat.ypos = mat.ypos / 10;
+    mat.ypos = mat.ypos / størrelse;
     mat.ypos = Math.floor(mat.ypos);
-    mat.ypos = mat.ypos * 10;
+    mat.ypos = mat.ypos * størrelse;
     score = score + 1;
     framerate = framerate + 1;
     frameRate(framerate);
   }
 
+  //Appear at the other end
   if (slange[0].xpos >= width) {
     slange[0].xpos = 0;
   } else if (slange[0].xpos < 0) {
@@ -97,12 +110,14 @@ function draw() {
     slange[0].ypos = height - størrelse;
   }
 
+  //Move forward
   slange.unshift(
     new Kroppsdel(slange[0].xpos + xhas, slange[0].ypos + yhas, størrelse)
   );
   slange.pop();
 }
 
+//Reset
 function keyPressed() {
   if (keyCode === 82) {
     frameRate(5);
@@ -112,6 +127,8 @@ function keyPressed() {
       new Kroppsdel(xstart - 2 * størrelse, ystart, størrelse, "blue"),
     ];
     score = 0;
+
+    //Input
   } else if ((keyCode === UP_ARROW || keyCode === 87) && yhas != størrelse) {
     xhas = 0;
     yhas = -størrelse;
